acb4571fbfd8fa192c8a6b25421c7dc8c884e5bd
xShackyx <meto.bissaro@hotmail.it>
2025-01-09 19:50:32 +0100

    modified card size and unit name background shadow

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ import { UnitProvider } from "../contexts/UnitContext.jsx";
 
 function Card({ selectedUnit, unitCustomName, unitSkill }) {
   return (
-    <div className="relative border-8 bg-white border-black z-10 w-full h-[27rem] flex flex-col">
+    <div className="relative border-8 bg-white border-black z-10 w-full flex flex-col">
       <UnitProvider unit={selectedUnit}>
         <CardHeader unitCustomName={unitCustomName} />
         <CardBody>
--- src/components/CardHeader.jsx
+++ src/components/CardHeader.jsx
@@ -6,8 +6,8 @@ function CardHeader({ unitCustomName }) {
   return (
     <div className="flex justify-between">
       <div className="flex flex-col px-1">
-        <p className="text-xl">{unitCustomName ? unitCustomName : ""}</p>
-        <p className="font-extrabold uppercase text-xl ">
+        <p className="text-xl ">{unitCustomName ? unitCustomName : ""}</p>
+        <p className="font-extrabold uppercase text-xl drop-shadow-[0_1px_1.5px_rgba(255,255,255)]">
           {unit ? unit.Name : "Unit Name"}
         </p>
       </div>
